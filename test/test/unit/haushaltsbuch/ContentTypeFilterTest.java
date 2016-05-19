e0634f07de8090c7e513c3665444a4f9a51ef9ec
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2016-05-20 00:10:35 +0200

    Set encoding only if not present

---
+++ test/test/unit/haushaltsbuch/ContentTypeFilterTest.java
@@ -0,0 +1,52 @@
+package test.unit.haushaltsbuch;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import org.junit.Before;
+import org.junit.Test;
+import haushaltsbuch.controllers.ContentTypeFilter;
+
+public class ContentTypeFilterTest
+{
+  private ContentTypeFilter _subject;
+
+  @Before
+  public void setUp() throws Exception
+  {
+    _subject = new ContentTypeFilter();
+  }
+
+  @Test
+  public void testKeepPresetCharacterEncoding() throws IOException, ServletException
+  {
+    ServletRequest request = mock(ServletRequest.class);
+    ServletResponse response = mock(ServletResponse.class);
+    FilterChain chain = mock(FilterChain.class);
+
+    when(request.getCharacterEncoding()).thenReturn("UTF-42");
+
+    _subject.doFilter(request, response, chain);
+
+    verify(request, times(0)).setCharacterEncoding(anyString());
+  }
+
+  @Test
+  public void testNoPresetCharacterEncoding() throws IOException, ServletException
+  {
+    ServletRequest request = mock(ServletRequest.class);
+    ServletResponse response = mock(ServletResponse.class);
+    FilterChain chain = mock(FilterChain.class);
+
+    _subject.doFilter(request, response, chain);
+
+    verify(request).setCharacterEncoding("UTF-8");
+  }
+}
