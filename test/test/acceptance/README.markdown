c06f6d55672c26e7d5c7efe07c4e9d7913652ed2
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2016-05-21 12:02:15 +0200

    Add acceptance tests

---
+++ test/test/acceptance/README.markdown
@@ -0,0 +1,18 @@
+# Acceptance Tests
+
+Uses [ruby bindings](https://github.com/SeleniumHQ/selenium/wiki/Ruby-Bindings) to [Selenium](https://seleniumhq.github.io)'s [https://sites.google.com/a/chromium.org/chromedriver/](chrome-driver).
+
+See also the [cheat sheet](https://gist.github.com/kenrett/7553278).
+
+## Installation
+
+```
+bundle install
+brew bundle # or use an alternative method to install chrome-driver
+```
+
+## Running Tests
+
+```
+bundle exec rake
+```
---
+++ test/test/acceptance/test_index.rb
@@ -0,0 +1,57 @@
+require 'minitest/autorun'
+require 'selenium-webdriver'
+
+#
+# Test the index page and navigation links
+#
+class TestIndex < MiniTest::Test
+  def setup
+    @driver = Selenium::WebDriver.for :chrome
+    @driver.navigate.to 'http://localhost:9080/hhb/'
+  end
+
+  def teardown
+    @driver.quit
+  end
+
+  def test_home
+    link_home = @driver.find_element(xpath: "//a[@href='.']")
+    assert(link_home.displayed?)
+
+    link_home.click
+
+    h1 = @driver.find_element(xpath: '/html/body/h1')
+    assert(h1.displayed?)
+  end
+
+  def test_new
+    link_new = @driver.find_element(xpath: "//a[@href='new']")
+    assert(link_new.displayed?)
+
+    link_new.click
+
+    form = @driver.find_element(xpath: '/html/body/form')
+    assert(form.displayed?)
+  end
+
+  def test_list
+    list = @driver.find_element(xpath: "//a[@href='all']")
+    assert(list.displayed?)
+
+    list.click
+
+    table = @driver.find_element(xpath: '/html/body/table')
+    assert(table.displayed?)
+  end
+
+  def test_title
+    assert_equal 'HDM Haushaltsbuch', @driver.title
+  end
+end
+
+__END__
+
+# element = driver.find_element(:name, 'q')
+# element.send_keys "Hello WebDriver!"
+# element.submit
+@driver.find_element(xpath: '/html/body/table/tbody/tr/td').displayed?
