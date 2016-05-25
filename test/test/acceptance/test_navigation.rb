177afc446f3929a992f3989067dde1f9fcd21767
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2016-05-25 22:09:25 +0200

    Extract common base class for tests

--- test/test/acceptance/test_navigation.rb
+++ test/test/acceptance/test_navigation.rb
@@ -1,71 +1,65 @@
-require 'minitest/autorun'
-require 'selenium-webdriver'
+require 'helpers'
 
 #
 # Test the navigation links
 #
-class TestNavigation < MiniTest::Test
-  def setup
-    @driver = Selenium::WebDriver.for :chrome
-    @driver.navigate.to 'http://localhost:9080/hhb/'
-  end
-
-  def teardown
-    @driver.quit unless failure
-  end
-
+class TestNavigation < AcceptanceTest
   def test_new
-    link_new = @driver.first(xpath: "//a[@href='new']")
+    driver.navigate.to 'http://localhost:9080/hhb/'
+    link_new = driver.first(xpath: "//a[@href='new']")
     assert(link_new.displayed?)
 
     link_new.click
 
-    form = @driver.first(xpath: '/html/body/form')
+    form = driver.first(xpath: '/html/body/form')
     assert(form.displayed?)
 
-    active_menu_item = @driver.first(css: '.navbar li .active')
+    active_menu_item = driver.first(css: '.navbar li .active')
     assert(active_menu_item.displayed?)
     assert_includes(active_menu_item.attribute('href'), 'new')
   end
 
   def test_all
-    all = @driver.first(xpath: "//a[@href='all']")
+    driver.navigate.to 'http://localhost:9080/hhb/'
+    all = driver.first(xpath: "//a[@href='all']")
     assert(all.displayed?)
 
     all.click
 
-    table = @driver.first(xpath: '/html/body/table')
+    table = driver.first(xpath: '/html/body/table')
     assert(table.displayed?)
 
-    active_menu_item = @driver.first(css: '.navbar li .active')
+    active_menu_item = driver.first(css: '.navbar li .active')
     assert(active_menu_item.displayed?)
     assert_includes(active_menu_item.attribute('href'), 'all')
   end
 
   def test_lookup
-    lookup = @driver.first(xpath: "//a[@href='lookup']")
+    driver.navigate.to 'http://localhost:9080/hhb/'
+    lookup = driver.first(xpath: "//a[@href='lookup']")
     assert(lookup.displayed?)
 
     lookup.click
 
-    form = @driver.first(xpath: '/html/body/form')
+    form = driver.first(xpath: '/html/body/form')
     assert(form.displayed?)
 
-    active_menu_item = @driver.first(css: '.navbar li .active')
+    active_menu_item = driver.first(css: '.navbar li .active')
     assert(active_menu_item.displayed?)
     assert_includes(active_menu_item.attribute('href'), 'lookup')
   end
 
   def test_home
-    link_home = @driver.first(xpath: "//a[@href='.']")
+    driver.navigate.to 'http://localhost:9080/hhb/'
+    link_home = driver.first(xpath: "//a[@href='.']")
     assert(link_home.displayed?)
 
     link_home.click
 
-    h1 = @driver.first(xpath: '/html/body/h1')
+    h1 = driver.first(xpath: '/html/body/h1')
     assert(h1.displayed?)
 
-    active_menu_item = @driver.first(css: '.navbar li .active')
+    active_menu_item = driver.first(css: '.navbar li .active')
     assert(active_menu_item.displayed?)
   end
 end
--- test/test/acceptance/test_new.rb
+++ test/test/acceptance/test_new.rb
@@ -1,26 +1,17 @@
-require 'minitest/autorun'
-require 'selenium-webdriver'
+require 'helpers'
 
 #
 # Test the 'new entry' page
 #
-class TestNew < MiniTest::Test
-  def setup
-    @driver = Selenium::WebDriver.for :chrome
-  end
-
-  def teardown
-    @driver.quit unless failure
-  end
-
+class TestNew < AcceptanceTest
   def test_new
     create
 
-    message = @driver.first(xpath: '//*[@id="message"]')
+    message = driver.first(xpath: '//*[@id="message"]')
     assert(message.displayed?)
     assert_includes(message.text, 'erfolgreich angelegt')
 
-    id = @driver.first(xpath: '//*[@class="id"]')
+    id = driver.first(xpath: '//*[@class="id"]')
     assert(id.displayed?)
 
     lookup(id.text)
@@ -31,47 +22,15 @@ class TestNew < MiniTest::Test
     umlauts = 'some umlauts: öäüß'
     create(description: umlauts)
 
-    description = @driver.first(css: '.description')
+    description = driver.first(css: '.description')
     assert(description.displayed?)
     assert_equal(umlauts, description.text)
 
-    id = @driver.first(xpath: '//*[@class="id"]')
+    id = driver.first(xpath: '//*[@class="id"]')
     assert(id.displayed?)
 
     lookup(id.text)
     delete
-  end
-
-  def create(fields = {})
-    @driver.navigate.to 'http://localhost:9080/hhb/new'
-
-    @driver.first(name: 'srcDst').send_keys(value(:srcDst, fields))
-    @driver.first(name: 'description').send_keys(value(:description, fields))
-    @driver.first(name: 'value').send_keys(42)
-    @driver.first(name: 'category').send_keys(value(:category, fields))
-    @driver.first(name: 'paymentType').send_keys(value(:paymentType, fields))
-
-    @driver.first(id: 'new').submit
-  end
-
-  def lookup(id)
-    @driver.navigate.to 'http://localhost:9080/hhb/lookup'
-
-    lookup_form = @driver.first(id: 'lookup')
-    assert(lookup_form.displayed?)
-    input = lookup_form.first(name: 'id')
-    assert(input.displayed?)
-    input.send_keys(id)
-    input.submit
-  end
-
-  def delete
-    delete_button = @driver.first(xpath: '//input[@name="id"]')
-    delete_button.submit
-
-    message = @driver.first(xpath: '//*[@id="message"]')
-    assert(message.displayed?)
-    assert_includes(message.text, 'gelöscht')
   end
 
   private
