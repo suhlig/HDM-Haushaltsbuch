78c1cad610019b4547f61dee93b93a9b1d9b9d8b
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2016-05-27 14:10:40 +0200

    Refactor repository initialization

---
+++ src/haushaltsbuch/web/ServletContextRegistry.java
@@ -0,0 +1,32 @@
+package haushaltsbuch.web;
+
+import java.sql.Connection;
+import javax.servlet.ServletContext;
+import haushaltsbuch.EntryRepository;
+import haushaltsbuch.web.controllers.BaseController;
+
+public class ServletContextRegistry
+{
+  private final static String CTX_ATTR_CONNECTION = ServletContextRegistry.class.getName() + "_DB_CONNECTION";
+  private final ServletContext _servletContext;
+
+  public ServletContextRegistry(ServletContext servletContext)
+  {
+    _servletContext = servletContext;
+  }
+
+  public Connection getConnection()
+  {
+    return (Connection) _servletContext.getAttribute(CTX_ATTR_CONNECTION);
+  }
+
+  public void register(Connection connection)
+  {
+    _servletContext.setAttribute(CTX_ATTR_CONNECTION, connection);
+  }
+
+  public void register(EntryRepository repository)
+  {
+    _servletContext.setAttribute(BaseController.CTX_ATTR_NAME, repository);
+  }
+}
