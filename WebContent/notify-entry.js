8a6888600977f0c637d4e5017663920a57e81cda
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2016-06-04 17:09:28 +0200

    Reconnect to SSE stream
    * Improve the behavior when the SSE stream is down from the beginning
    * Also use exponential backoff to not overload the client

--- WebContent/notify-entry.js
+++ WebContent/notify-entry.js
@@ -1,6 +1,5 @@
-var es = new EventSource('http://localhost:4567/stream');
 var Notify = window.Notify.default;
-  
+
 function onShowNotification () {
   // console.log('notification is shown!');
 }
@@ -36,18 +35,53 @@ function doNotification(event) {
     notifyError: onErrorNotification,
     timeout: 10
   });
-  
+
   myNotification.show();
 }
 
-es.onmessage = function(e) { 
-  var event = JSON.parse(e.data);
-  
-  console.log(event);
-  
-  if (!Notify.needsPermission) {
-      doNotification(event);
-  } else if (Notify.isSupported()) {
-      Notify.requestPermission(onPermissionGranted(event), onPermissionDenied(event));
+/** lookup constant name from value */
+EventSource.prototype.byIndex = function(i){
+  var ii = 0;
+
+  for (var lbl in this){
+    if (ii == i)
+      return lbl;
+
+    ii += 1;
   }
 };
+
+function initEventSource(attempt = 0) {
+  var es = new EventSource("http://localhost:4567/stream");
+  console.info('Attempt #' + attempt + ' connecting to ' + es.url);
+
+  es.onerror = function(e) {
+    if (es.readyState != EventSource.CONNECTING) {
+      console.warn('EventSource error; state is ' + es.byIndex(es.readyState));
+    } else {
+      es.close();
+      backoff = Math.pow(2, attempt++);
+      console.log('Error establishing EventSource connection to ' + es.url + '; trying again in ' + backoff + ' seconds');
+      setTimeout(initEventSource, 1000 * backoff, attempt);
+    }
+  };
+
+  es.onopen = function(e) {
+    attempt = 0;
+    console.log('Successfully established EventSource connection to ' + es.url);
+  };
+
+  es.onmessage = function(event) {
+    console.info(event);
+
+    var doc = JSON.parse(event.data);
+
+    if (!Notify.needsPermission) {
+        doNotification(doc);
+    } else if (Notify.isSupported()) {
+        Notify.requestPermission(onPermissionGranted(doc), onPermissionDenied(doc));
+    }
+  };
+};
+
+initEventSource();
